7826867cb13778341f0d830903dad37f87f3a7e1
Niki I. Zarandi <Zarandi@Nikis-MacBook-Pro.local>
2023-01-23 14:55:36 +0100

    Scaffold react meme generator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
+import { saveAs } from 'file-saver';
+//import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+
+  const [topText, setTopText] = useState('');
+  const [bottomText, setBottomText] = useState('');
+  const [MemeTemplate, setMemeTemplate] = useState('');
+  const [memes, setMemes] = useState([]);
+
+function handleTopChange(e) {
+  setTopText(e);
 }
 
-export default App;
+function handleBottomChange(e) {
+  setBottomText(e);
+}
+
+function handleMemeTemplateChange(e) {
+  setMemeTemplate(e);
+}
+
+  return (
+    <main>
+      <section>
+          <div>
+          <h1 className='name'>MEMES GENERATOR</h1>
+
+
+  <br />
+
+    <label>topText
+          <input
+            type='text'
+              className='input'
+                name='top text'
+                value={topText}
+                  placeholder='top text'
+                onChange={(e) => handleTopChange(e.target.value)}
+  />
+    </label>
+
+  <br />
+  <br />
+
+    <label>bottomText
+        <input
+          type='text'
+            className='input'
+              name='bottom text'
+                value={bottomText}
+                    placeholder='bottomText'
+          onChange={(e) => handleBottomChange(e.target.value)}
+  />
+    </label>
+
+  <br />
+  <br />
+
+    <label>meme template
+        <input
+            type='text'
+              className='meme template'
+                placeholder='meme template'
+            onChange={(e) => handleMemeTemplateChange(e.target.value)}
+  />
+    </label>
+
+
+    <button
+       onClick={()=>
+       saveAs (`https://api.memegen.link/images/${MemeTemplate}/${topText}/${bottomText}`, 'image.jpg'
+       )
+    }
+      >
+      download
+    </button>
+
+                <br />
+                <br />
+                <br />
+            <img
+            data-test-id="meme-image"
+              src={`https://api.memegen.link/images/${MemeTemplate}/${topText}/${bottomText}`}
+            alt='memes'
+              />
+            </div>
+        </section>
+      </main>
+  );
+ }
